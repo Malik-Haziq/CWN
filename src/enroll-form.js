10ef982762eaa21c1c5400168bdcf24a44730a5a
Malik-Haziq <malikhaziq153@gmail.com>
2023-07-24 17:03:57 +0500

    updates

--- src/enroll-form.js
+++ src/enroll-form.js
@@ -2,40 +2,18 @@ const enrollmentForm = document.querySelector("#enrollment-form");
 const enrollmentInputFields = document.querySelectorAll(
   "#enrollment-form input, #enrollment-form select"
 );
-let inputs = [];
-let obj = [];
 
+let formData = [];
+let dataArr = [];
 enrollmentForm.addEventListener("submit", (e) => {
   e.preventDefault();
   inputs = enrollmentInputFields.forEach((field, i) => {
-    obj = { [`${field.dataset.name}`]: field.value };
-    inputs.push(obj);
+    formData.push({ [`${field.dataset.name}`]: field.value });
   });
-  for (let i = 0; i < inputs.length; i++) {
-    Object.assign(inputs, inputs[i]);
-  }
-  console.log(inputs);
+
+  const singleObject = formData.reduce((accumulator, currentObject) => {
+    return { ...accumulator, ...currentObject };
+  }, {});
+  dataArr.push(singleObject);
+  console.log(dataArr);
 });
-
-// function handleSubmit(event) {
-//   event.preventDefault(); // Prevent form submission (we'll handle it)
-
-//   // Get the form element
-// const form = event.target;
-// const formData = new FormData(form); // Create a FormData object to get form values
-// console.log(formData);
-// // Convert the FormData object to a plain JavaScript object
-// const formValues = {};
-// formData.forEach((value, name) => {
-//   formValues[name] = value;
-// });
-
-//   // Now, you have an object with all the form values
-//   console.log(formValues);
-
-//   // If you want to do something else with the object, you can pass it to another function or process it here
-//   // For example: sendDataToServer(formValues);
-// }
-
-// // Add an event listener to the form to call the handleSubmit function on form submission
-// enrollmentForm.addEventListener("submit", handleSubmit);
