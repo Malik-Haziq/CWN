872fea49051614e877a8ab4f95496e0388cdb0c8
Malik Haziq <malikhaziq153@gmail.com>
2023-08-06 11:55:41 -0700

    navigation done

--- src/script.js
+++ src/script.js
@@ -1,13 +1,33 @@
-const userEmails = [];
 
+const menuBar = document.querySelector('.menu-bar')
+const xMark = document.querySelector('.xmark')
+const nav = document.querySelector('.nav-links')
 const notifyForm = document.querySelector(".secondary-cta form");
 const notifyInput = document.querySelector(".secondary-cta form input");
 const notifyModal = document.querySelector("section:has(.overlay)");
 const notifyModalCross = document.querySelector(".thanks-msg img");
 const notifyModalOverlay = document.querySelector(".overlay");
-
 const skillName = document.querySelector(".skill-name");
 
+const userEmails = [];
+
+// ===================  Navigation bar
+menuBar.addEventListener('click',()=>{
+  menuBar.classList.add('hidden')
+  xMark.classList.remove('hidden')
+
+  nav.style.transform = 'translateY(0)';
+
+
+})
+
+xMark.addEventListener('click',()=>{
+  menuBar.classList.remove('hidden')
+  xMark.classList.add('hidden')
+
+  nav.style.transform = 'translateY(-550px)';
+
+})
 // --------- Events
 
 notifyForm.addEventListener("submit", (e) => {
